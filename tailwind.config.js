4acbe47199298fe30b47db9b17b548bb5fe34843
Julien Viala <julien.viala@ekino.com>
2021-11-06 13:47:24 +0100

    feat: index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
@@ -9,11 +11,18 @@ module.exports = {
     container: {
       center: true,
     },
+    boxShadow: theme => ({
+      ...theme.boxShadow,
+      bubble: `4px 4px 0px 0px ${theme('colors.canary')}`
+    }),
     extend: {
       colors: {
         downriver: "#08124f",
         blue: "#0f24f7",
+        selago: "#f4f6fe",
+        canary: "#e3fd52"
       },
+
     },
   },
 };
