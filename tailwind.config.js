a625a3308ac40073ef0d18ecd965ba4cb5f06a45
Julien Viala <julien.viala@ekino.com>
2021-11-14 17:49:24 +0100

    feat: launch button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
@@ -11,15 +11,15 @@ module.exports = {
     container: {
       center: true,
     },
-    boxShadow: theme => ({
+    boxShadow: (theme) => ({
       ...theme.boxShadow,
-      bubble: `4px 4px 0px 0px ${theme('colors.canary')}`,
-      buttonTypePrimary: `0px 4px 0px 0px ${theme('colors.downriver')}`,
-      buttonTypePrimary_focused: `0px 2px 0px 0px ${theme('colors.downriver')}`
+      bubble: `4px 4px 0px 0px ${theme("colors.canary")}`,
+      buttonTypePrimary: `0px 4px 0px 0px ${theme("colors.downriver")}`,
+      buttonTypePrimary_focused: `0px 2px 0px 0px ${theme("colors.downriver")}`,
     }),
-    outline: theme => ({
+    outline: (theme) => ({
       ...theme.outline,
-      button: [`2px solid ${theme('colors.denim')}`, '4px'],
+      button: [`2px solid ${theme("colors.denim")}`, "4px"],
     }),
     extend: {
       colors: {
