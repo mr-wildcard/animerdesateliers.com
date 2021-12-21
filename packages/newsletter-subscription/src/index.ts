25c9203b232addcf9ac12a18121ab5fe60bb5e8e
Julien Viala <dev.vialajulien@gmail.com>
2021-12-21 12:43:59 +0100

    feat: (POC) add contact to newsletter

--- packages/newsletter-subscription/src/index.ts
+++ packages/newsletter-subscription/src/index.ts
@@ -1,6 +1,32 @@
-import { handleRequest } from './handler'
+// mailjet.connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
-addEventListener('fetch', (event) => {
-  if (event.request.url )
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleResponse());
+});
+
+async function handleResponse() {
+  return fetch("https://api.mailjet.com/v3/REST/contact", {
+    body: JSON.stringify({
+      IsExcludedFromCampaigns: "true",
+      Name: "New Contact",
+      Email: "passenger@mailjet.com",
+    }),
+    headers: {
+      Authorization: `Basic ${btoa(`${MJ_APIKEY_PUBLIC}:${MJ_APIKEY_PRIVATE}`)}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+}
+/*
+curl -s \
+	-X POST \
+	--user "$MJ_APIKEY_PUBLIC:$MJ_APIKEY_PRIVATE" \
+	https://api.mailjet.com/v3/REST/contact \
+	-H 'Content-Type: application/json' \
+	-d '{
+      "IsExcludedFromCampaigns":"true",
+      "Name":"New Contact",
+      "Email":"passenger@mailjet.com"
+	}'
+ */
--- packages/newsletter-subscription/webpack.config.js
+++ packages/newsletter-subscription/webpack.config.js
@@ -1,26 +1,33 @@
-const path = require('path')
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'worker.js',
-    path: path.join(__dirname, 'dist'),
+    filename: "worker.js",
+    path: path.join(__dirname, "dist"),
   },
-  devtool: 'cheap-module-source-map',
-  mode: 'development',
+  devtool: "cheap-module-source-map",
+  mode: "development",
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         options: {
           // transpileOnly is useful to skip typescript checks occasionally:
-          // transpileOnly: true,
+          transpileOnly: true,
         },
       },
     ],
   },
-}
+  plugins: [
+    new webpack.DefinePlugin({
+      MJ_APIKEY_PUBLIC: JSON.stringify(process.env.MJ_APIKEY_PUBLIC),
+      MJ_APIKEY_PRIVATE: JSON.stringify(process.env.MJ_APIKEY_PRIVATE),
+    }),
+  ],
+};
