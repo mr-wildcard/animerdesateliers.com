62942b375039240f4347e8915a272fa7eb23155c
Julien Viala <dev.vialajulien@gmail.com>
2021-12-21 14:32:52 +0100

    chore: dynamic astro buildOptions site

--- packages/website/astro.config.js
+++ packages/website/astro.config.js
@@ -3,12 +3,26 @@ const tailwindConfig = require("./tailwind.config.js");
 
 const fullConfig = resolveConfig(tailwindConfig);
 
-// @ts-check
+function getSiteURL() {
+  if (process.env.CF_PAGES_BRANCH) {
+    if (process.env.CF_PAGES_BRANCH !== "main") {
+      return "https://animerdesateliers.pages.dev/";
+    } else {
+      return "https://animerdesateliers.com/";
+    }
+  } else {
+    return "http://localhost:3000/";
+  }
+}
+
 module.exports = {
   renderers: [],
   devOptions: {
     tailwindConfig: "./tailwind.config.js",
     hostname: "0.0.0.0",
+  },
+  buildOptions: {
+    site: getSiteURL(),
   },
   vite: {
     define: {
