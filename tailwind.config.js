ab4c9d4ff7913ce75baabcc1b5f20db9897fca4f
Julien Viala <dev.vialajulien@gmail.com>
2021-12-21 12:43:44 +0100

    feat(button): component

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,16 +13,23 @@ module.exports = {
     },
     boxShadow: theme => ({
       ...theme.boxShadow,
-      bubble: `4px 4px 0px 0px ${theme('colors.canary')}`
+      bubble: `4px 4px 0px 0px ${theme('colors.canary')}`,
+      buttonTypePrimary: `0px 4px 0px 0px ${theme('colors.downriver')}`,
+      buttonTypePrimary_focused: `0px 2px 0px 0px ${theme('colors.downriver')}`
+    }),
+    outline: theme => ({
+      ...theme.outline,
+      button: [`2px solid ${theme('colors.denim')}`, '4px'],
     }),
     extend: {
       colors: {
         downriver: "#08124f",
         blue: "#0f24f7",
+        denim: "#197abf",
         selago: "#f4f6fe",
-        canary: "#e3fd52"
+        canary: "#e3fd52",
+        chiffon: "#f7fecb",
       },
-
     },
   },
 };
