d54381413cafea56d82767402f78cde4ec21b839
Julien Viala <dev.vialajulien@gmail.com>
2022-01-04 13:40:22 +0100

    feat: sticky header behavior on md lg xl

---
+++ packages/website/src/components/header-sticky/index.ts
@@ -0,0 +1,83 @@
+enum ScrollDirection {
+  UP,
+  DOWN,
+}
+
+(function () {
+  window.addEventListener("DOMContentLoaded", (event) => {
+    const stickyHeader = document.querySelector<HTMLDivElement>("#sticky-header");
+    let { height: stickyHeaderHeight } = stickyHeader.getBoundingClientRect();
+    let previous_known_scroll_position = window.scrollY;
+    let last_known_scroll_position = window.scrollY;
+    let scrollAmount = 0;
+    let ticking = false;
+    let currentScrollDirection: ScrollDirection;
+
+    function onScroll() {
+      if (!ticking) {
+        ticking = true;
+
+        window.requestAnimationFrame(function () {
+          previous_known_scroll_position = last_known_scroll_position;
+          last_known_scroll_position = window.scrollY;
+
+          const scrollDirection: ScrollDirection =
+            last_known_scroll_position > previous_known_scroll_position ? ScrollDirection.DOWN : ScrollDirection.UP;
+
+          scrollAmount -= last_known_scroll_position - previous_known_scroll_position;
+
+          if (scrollDirection === ScrollDirection.UP) {
+            scrollAmount = Math.min(scrollAmount, 0);
+          } else {
+            /**
+             * -1 : hide the remaining 1px border bottom of sticky header.
+             */
+            scrollAmount = Math.max(scrollAmount, -stickyHeaderHeight - 1);
+          }
+
+          stickyHeader.style.transform = `translateY(${scrollAmount}px)`;
+
+          ticking = false;
+        });
+      }
+    }
+
+    /**
+     * Observe the "Me prevenir du lancement" button in the header.
+     */
+    const headerNewsletterAnchor = document.querySelector<HTMLDivElement>("#header-newsletter-anchor");
+
+    const headerNewsletterAnchorIO = new IntersectionObserver((entries) => {
+      const [newsletterAnchor] = entries;
+
+      if (newsletterAnchor.isIntersecting) {
+        window.removeEventListener("scroll", onScroll);
+
+        stickyHeader.classList.add("overlapping-header");
+        stickyHeader.style.transform = "";
+        scrollAmount = -stickyHeaderHeight;
+        previous_known_scroll_position = window.scrollY;
+        last_known_scroll_position = window.scrollY;
+      } else {
+        stickyHeader.classList.remove("overlapping-header");
+
+        window.addEventListener("scroll", onScroll, { passive: true });
+      }
+    });
+
+    headerNewsletterAnchorIO.observe(headerNewsletterAnchor);
+
+    /**
+     * Observe the size changes of the sticky header element.
+     */
+    const stickyHeaderRO = new ResizeObserver((entries) => {
+      const [stickyHeader] = entries;
+
+      let { blockSize } = stickyHeader.borderBoxSize[0];
+
+      stickyHeaderHeight = blockSize;
+    });
+
+    stickyHeaderRO.observe(stickyHeader);
+  });
+})();
