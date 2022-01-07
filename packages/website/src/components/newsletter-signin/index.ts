50ca875cf058dc5309fed5ad275e873097949903
Julien Viala <dev.vialajulien@gmail.com>
2022-01-07 20:52:08 +0100

    feat: migrate to mailchimp

--- packages/website/src/components/newsletter-signin/index.ts
+++ packages/website/src/components/newsletter-signin/index.ts
@@ -1,61 +1,62 @@
 // @ts-ignore
 import { proxy, subscribe } from "valtio";
 
+interface State {
+  httpStatus: null | number;
+  loading: boolean;
+  invalidEmail: boolean;
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
   document.querySelectorAll<HTMLFormElement>(".newsletter-form").forEach((htmlFormElement) => {
     const submitButton = htmlFormElement.querySelector("button.pushable");
     const invalidEmailMessage = htmlFormElement.querySelector('[data-validation-type="invalid-email"]');
     const emailInput = htmlFormElement.querySelector<HTMLInputElement>("input[type=email]");
 
-    const state = proxy({
-      statuses: {
-        loading: false,
-        success: false,
-        failed: false,
-      },
-      errors: {
-        invalidEmail: getEmailInputValueIsValid(),
-      },
+    const state = proxy<State>({
+      httpStatus: null,
+      loading: false,
+      invalidEmail: getEmailInputValueIsValid(),
     });
 
-    subscribe(state.errors, () => {
-      if (state.errors.invalidEmail) {
+    subscribe(state, () => {
+      if (state.invalidEmail) {
         toggleInputValidationMessage(true);
       } else {
         toggleInputValidationMessage(false);
       }
     });
 
-    subscribe(state.statuses, () => {
-      if (state.statuses.loading) {
+    subscribe(state, () => {
+      if (state.loading) {
         toggleButtonLoadingState(true);
       } else {
         toggleButtonLoadingState(false);
       }
     });
 
-    subscribe(state.statuses, () => {
-      if (!state.statuses.loading) {
-        if (state.statuses.success) {
-          displayValidationHTML(true);
+    subscribe(state, () => {
+      if (!state.loading && state.httpStatus !== null) {
+        if (state.httpStatus === 409 || state.httpStatus < 400) {
           emailInput.value = "";
-        } else if (state.statuses.failed) {
-          displayValidationHTML(false);
+          emailInput.checkValidity();
         }
+
+        displayValidationHTML();
       }
     });
 
     ["input", "focus", "blur"].forEach((event) => {
       emailInput.addEventListener(event, (e) => {
-        state.errors.invalidEmail = !getEmailInputValueIsValid();
+        state.invalidEmail = !getEmailInputValueIsValid();
       });
     });
 
     htmlFormElement.addEventListener("submit", (e) => {
       e.preventDefault();
 
-      if (state.statuses.loading || state.errors.invalidEmail) {
-        if (state.errors.invalidEmail) {
+      if (state.loading || state.invalidEmail) {
+        if (state.invalidEmail) {
           toggleInputValidationMessage(true);
         }
 
@@ -66,7 +67,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
 
     function submitForm() {
-      state.statuses.loading = true;
+      state.loading = true;
 
       fetch("/subscribe", {
         method: "POST",
@@ -78,17 +79,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
         },
       })
         .then((response) => {
-          state.statuses.loading = false;
-
-          if (response.ok) {
-            state.statuses.success = true;
-          } else {
-            state.statuses.failed = true;
-          }
+          state.httpStatus = response.status;
+          state.loading = false;
         })
         .catch((error) => {
-          state.statuses.loading = false;
-          state.statuses.failed = true;
+          state.httpStatus = 500;
+          state.loading = false;
         });
     }
 
@@ -112,11 +108,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
       return !emailInput.value.length || (emailInput.value.length && emailInput.checkValidity());
     }
 
-    function displayValidationHTML(success: boolean) {
+    function displayValidationHTML() {
       htmlFormElement.style.display = "none";
       htmlFormElement.style.opacity = "0";
 
-      const htmlContent = getValidationHTML(success);
+      const htmlContent = getValidationHTML();
       const wrapper = document.createElement("div");
       wrapper.setAttribute("role", "status");
       wrapper.style.opacity = "0";
@@ -134,9 +130,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
         () => {
           wrapper.remove();
 
-          state.statuses.loading = false;
-          state.statuses.success = false;
-          state.statuses.failed = false;
+          state.httpStatus = null;
+          state.loading = false;
 
           htmlFormElement.style.display = "block";
 
@@ -148,7 +143,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
       );
     }
 
-    function getValidationHTML(success: boolean) {
+    function getValidationHTML() {
+      const { httpStatus } = state;
+
       const buttonCSSClassnames = [
         "w-full",
         "text-lg",
@@ -166,27 +163,31 @@ window.addEventListener("DOMContentLoaded", (event) => {
         "md:w-auto",
         "hover:bg-chiffon",
         "hover:text-downriver",
-      ];
+      ].join(" ");
 
       return `
         <p class="text-2xl leading-[34px] mb-4 text-blue font-bold lg:text-4xl lg:leading-[56px]">
           ${
-            success
+            httpStatus < 400
               ? "✅ Cool, votre adresse email est enregistrée. À très bientôt !"
+              : httpStatus === 409
+              ? "✅ Pas d’inquiétude, votre adresse email est déjà dans notre liste !"
               : "❌ Petit souci technique... L’inscription n’a pas fonctionné."
           }
         </p>
 
         <p class="text-lg mb-10">
-          ${
-            success
-              ? "On vous a envoyé un email de confirmation où l’on vous demande votre avis sur le futur prix du cours en ligne."
-              : "Réessayez un peu plus tard ou écrivez-nous à <a href='mailto:contact@animerdesateliers.com' class='underline'>contact@animerdesateliers.com</a>."
-          }
+        ${
+          httpStatus < 400
+            ? "On vous a envoyé un email de confirmation où l’on vous demande votre avis sur le futur prix du cours en ligne."
+            : httpStatus === 409
+            ? "Ça, c’est de la motivation. À très bientôt !"
+            : "Réessayez un peu plus tard ou écrivez-nous à <a href='mailto:contact@animerdesateliers.com' class='underline'>contact@animerdesateliers.com</a>."
+        }
         </p>
 
-        <button class="${buttonCSSClassnames.join(" ")}">
-          ${success ? "Bien compris, je checke mes emails" : "Revenir à l’étape précédente"}
+        <button class="${buttonCSSClassnames}">
+        ${httpStatus < 400 ? "Bien compris, je checke mes emails" : "Revenir à l’étape précédente"}
         </button>
       `;
     }
--- packages/workers/newsletter-subscription/src/index.ts
+++ packages/workers/newsletter-subscription/src/index.ts
@@ -1,54 +1,122 @@
-declare var MJ_LIST_ID: string;
-declare var MJ_APIKEY_PUBLIC: string;
-declare var MJ_APIKEY_PRIVATE: string;
+declare var MC_API_KEY: string;
+declare var MC_API_KEY_LABEL: string;
+declare var MC_LIST_ID: string;
+declare var MC_SERVER_PREFIX: string;
 
-/**
- * https://dev.mailjet.com/email/reference/contacts/subscriptions/
- */
+interface HTTPBodyRequest {
+  email: string;
+}
+
+interface MailChimpResponseError {
+  title: string;
+  status: number;
+  detail: string;
+  instance: string;
+}
+
+async function md5(message: string) {
+  // encode as (utf-8) uint8array
+  const msgUint8 = new TextEncoder().encode(message);
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest("MD5", msgUint8);
+
+  // convert buffer to byte array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  // convert bytes to hex string
+  return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+}
+
 addEventListener("fetch", (event) => {
   if (event.request.method !== "POST") {
-    event.respondWith(new Response("", { status: 405 }));
+    event.respondWith(new Response("Only POST requests allowed.", { status: 405 }));
   } else if (!event.request.headers.get("content-type")?.includes("application/json")) {
-    event.respondWith(new Response("", { status: 415 }));
+    event.respondWith(new Response("HTTP request Content-type must be JSON.", { status: 415 }));
   } else {
-    event.respondWith(handleResponse(event.request));
+    event.respondWith(
+      event.request
+        .json()
+        .then((jsonBody) => {
+          const { email } = jsonBody as HTTPBodyRequest;
+
+          if (!email) {
+            return new Response("`email` key is missing in JSON", { status: 400 });
+          } else if (!email.length) {
+            return new Response("Email address value must not be empty.", { status: 400 });
+          } else {
+            return addEmailToNewsletterRequest(email);
+          }
+        })
+        .catch((error) => {
+          return new Response(
+            "An unknown error occurred while retrieving the email address from HTTP request on Cloudflare Worker side.",
+            {
+              status: 500,
+            }
+          );
+        })
+    );
   }
 });
 
-async function handleResponse(request: Request) {
-  const { email } = await request.json();
+async function addEmailToNewsletterRequest(email: string) {
+  try {
+    const hashedEmail = await md5(email);
 
-  if (!email.length) {
-    return new Response("You need to provide an email address", { status: 400 });
-  }
+    const response = await fetch(
+      `https://${MC_SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${MC_LIST_ID}/members/${hashedEmail}`,
+      {
+        body: JSON.stringify({
+          email_address: email,
+          email_type: "html",
+          status_if_new: "pending",
+          status: "pending",
+        }),
+        headers: {
+          Authorization: `Basic ${btoa(`${MC_API_KEY_LABEL}:${MC_API_KEY}`)}`,
+        },
+        method: "PUT",
+      }
+    );
 
-  return fetch(`https://api.mailjet.com/v3/REST/contactslist/${MJ_LIST_ID}/managecontact`, {
-    body: JSON.stringify({
-      Action: "addforce",
-      Email: email,
-    }),
-    headers: {
-      Authorization: `Basic ${btoa(`${MJ_APIKEY_PUBLIC}:${MJ_APIKEY_PRIVATE}`)}`,
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-  })
-    .then((response) => {
-      if (response.ok) {
-        return new Response(`Email address ${email} successfully added to newsletter subscription !`);
-      } else {
-        console.error("An error occured while contacting MailJet.", "Submitted email:", email);
+    if (response.ok) {
+      return new Response(`Email address ${email} successfully added to newsletter !`, {
+        status: response.status,
+      });
+    } else {
+      try {
+        const mailChimpError = (await response.json()) as MailChimpResponseError;
 
-        return new Response(`An error occured while adding ${email} address to newsletter subscription.`, {
+        console.error(
+          `An error occurred while contacting MailChimp. Submitted email: ${email}`,
+          JSON.stringify(mailChimpError, null, 2)
+        );
+
+        if (mailChimpError.status === 400 && mailChimpError.title === "Member Exists") {
+          return new Response("Email address has already been subscribed to newsletter.", {
+            status: 409,
+          });
+        } else {
+          return new Response(`An error occurred while contacting MailChimp. Submitted email: ${email}`, {
+            status: response.status,
+          });
+        }
+      } catch (error) {
+        const mailChimpError = await response.text();
+
+        console.error(`An error occurred while contacting MailChimp. Submitted email: ${email}`, mailChimpError);
+
+        return new Response(`An error occurred while subscribing email ${email} to newsletter.`, {
           status: response.status,
         });
       }
-    })
-    .catch((error) => {
-      console.error("An error occured while contacting MailJet", error);
+    }
+  } catch (error) {
+    console.error("An unknown error occurred.", error);
 
-      return new Response(`An error occured while adding ${email} address to newsletter subscription.`, {
-        status: 500,
-      });
+    return new Response("An unknown error occurred :(", {
+      status: 500,
     });
+  }
 }
