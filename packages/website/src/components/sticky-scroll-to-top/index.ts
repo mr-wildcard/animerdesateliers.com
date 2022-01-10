53c99143a11d97035424cba38cf85d8ad7ef1535
Julien Viala <dev.vialajulien@gmail.com>
2022-01-10 12:16:47 +0100

    feat: scroll to top arrow

---
+++ packages/website/src/components/sticky-scroll-to-top/index.ts
@@ -0,0 +1,34 @@
+import { ScrollDirection, watchScrollDirection } from "scripts/watchScrollDirection";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const scrollToTopButton = document.querySelector("#scroll-to-top-button");
+  const siteHeader = document.querySelector<HTMLElement>("#site-header");
+  let headerIsVisible = true;
+
+  function onScroll(scrollDirection: ScrollDirection, scrollAmount: number) {
+    const buttonShouldBeVisible = !headerIsVisible && scrollDirection === ScrollDirection.UP;
+    const buttonShouldBeHidden = headerIsVisible || scrollDirection === ScrollDirection.DOWN;
+
+    if (buttonShouldBeVisible && !scrollToTopButton.classList.contains("show")) {
+      scrollToTopButton.classList.add("show");
+    } else if (buttonShouldBeHidden && scrollToTopButton.classList.contains("show")) {
+      scrollToTopButton.classList.remove("show");
+    }
+  }
+
+  const heroIO = new IntersectionObserver((entries) => {
+    const [heroElement] = entries;
+
+    headerIsVisible = heroElement.isIntersecting;
+  });
+
+  heroIO.observe(siteHeader);
+
+  const removeScrollListener = watchScrollDirection(onScroll);
+
+  scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+    });
+  });
+});
---
+++ packages/website/src/scripts/watchScrollDirection.ts
@@ -0,0 +1,31 @@
+export enum ScrollDirection {
+  UP,
+  DOWN,
+}
+
+type Callback = (scrollDirection: ScrollDirection, scrollAmount: number) => void;
+
+export function watchScrollDirection(callback: Callback): () => void {
+  let previous_known_scroll_position = window.scrollY;
+  let last_known_scroll_position = window.scrollY;
+  let currentScrollDirection: ScrollDirection;
+  let scrollAmount = 0;
+
+  function onScroll() {
+    previous_known_scroll_position = last_known_scroll_position;
+    last_known_scroll_position = window.scrollY;
+
+    const scrollDirection: ScrollDirection =
+      last_known_scroll_position > previous_known_scroll_position ? ScrollDirection.DOWN : ScrollDirection.UP;
+
+    scrollAmount -= last_known_scroll_position - previous_known_scroll_position;
+
+    callback(scrollDirection, scrollAmount);
+  }
+
+  window.addEventListener("scroll", onScroll);
+
+  return function dispose() {
+    window.removeEventListener("scroll", onScroll);
+  };
+}
