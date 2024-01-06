3d1c3f6f6bfce001bd0619d061617d3f6cb250ea
Julien Viala <julien.viala@shine.fr>
2024-01-06 09:59:20 +0100

    chore deduplicate base styles

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -24,7 +24,11 @@ export default {
     hostname: "0.0.0.0",
     port: 3000,
   },
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   site: getSiteURL(),
   vite: {
     define: {
