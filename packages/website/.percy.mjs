5bb80ec77ff6326f7cda482734fedf985a40ef69
Julien Viala <julien.viala@shine.fr>
2026-08-13 19:29:27 +0200

    fix(percy): lazy load images

--- packages/website/.percy.mjs
+++ packages/website/.percy.mjs
@@ -7,7 +7,30 @@ const pages = [
   { path: "/mentions-legales", name: "mentions-legales" },
 ];
 
+const basicSnapshotConfig = {
+  enableJavaScript: true,
+  async execute() {
+    document.querySelectorAll("summary").forEach((summary) => summary.click());
+
+    await new Promise((resolve) => {
+      window.addEventListener(
+        "scrollend",
+        function () {
+          resolve();
+        },
+        { once: true },
+      );
+
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth",
+      });
+    });
+  },
+};
+
 export default pages.map(({ path, name }) => ({
+  ...basicSnapshotConfig,
   name,
   url: `${PREVIEW_URL}${path}`,
 }));
